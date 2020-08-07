5b5725d199701f1f6e2b88ba2b217663d1a12a93
actondev <chris.actondev@gmail.com>
2020-08-07 19:15:40 +0200

    added basic textarea & eval button, upstream s7 with emscripten

--- src/s7_wasm.c
+++ src/s7_wasm.c
@@ -2,14 +2,22 @@
 
 #include <stdio.h>
 
-void _s7_print_stderr(s7_scheme *sc, uint8_t c, s7_pointer port) {
+void _s7_print_stdout(s7_scheme *sc, uint8_t c, s7_pointer port) {
     printf("%c", c);
 }
 
+s7_scheme* g_sc = NULL;
+
 int main() {
-  printf("hello, world!\n");
-  s7_scheme* sc = s7_init();
-  s7_set_current_output_port(sc, s7_open_output_function(sc, _s7_print_stderr));
-  s7_eval_c_string(sc, "(format #t \"result is ~A\n\" (+ 1 2 3))");
-  return 0;
+    printf("hello, world!\n");
+    g_sc = s7_init();
+    s7_set_current_output_port(g_sc, s7_open_output_function(g_sc, _s7_print_stdout));
+    s7_eval_c_string(g_sc, "(format #t \"result is ~A\n\" (+ 1 2 3))");
+    return 0;
+}
+
+// exported to wasm
+void eval_string(char* str) {
+    printf("called eval string with arg %s\n", str);
+    s7_eval_c_string(g_sc, str);
 }
