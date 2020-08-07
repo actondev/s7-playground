ae79d3a6620599796801fad97c1e3fac4cd124ee
actondev <chris.actondev@gmail.com>
2020-08-07 19:25:33 +0200

    added demo gif

--- src/s7_wasm.c
+++ src/s7_wasm.c
@@ -12,12 +12,12 @@ int main() {
     printf("hello, world!\n");
     g_sc = s7_init();
     s7_set_current_output_port(g_sc, s7_open_output_function(g_sc, _s7_print_stdout));
-    s7_eval_c_string(g_sc, "(format #t \"result is ~A\n\" (+ 1 2 3))");
+    // s7_eval_c_string(g_sc, "(format #t \"result is ~A\n\" (+ 1 2 3))");
     return 0;
 }
 
 // exported to wasm
 void eval_string(char* str) {
-    printf("called eval string with arg %s\n", str);
+    // printf("called eval string with arg %s\n", str);
     s7_eval_c_string(g_sc, str);
 }
