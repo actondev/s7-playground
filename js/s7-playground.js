69515ecad2c9de109be5e8a81b2fb0ff1cd2aac9
actondev <chris.actondev@gmail.com>
2020-09-26 01:50:26 +0200

    Feature/index.org (#2)

---
+++ js/s7-playground.js
@@ -0,0 +1,58 @@
+function setup() {
+    const codeElements = document.getElementsByClassName("code");
+    // looping in reverse. if not, replacing the code elements with codemirror will not work
+    for (var i = codeElements.length-1; i >= 0; i--) {
+	const codeElement = codeElements[i];
+	const parent = codeElement.parentNode;
+	const cm = CodeMirror(function(elt) {
+	    codeElement.parentNode.replaceChild(elt, codeElement);
+	}, {value: codeElement.value,
+	    // lineNumbers: true,
+	    mode: "scheme",
+	    theme: "monokai",
+	    styleActiveLine: true,
+	    autoCloseBrackets: true,
+	    matchBrackets: true,
+	   });
+	parinferCodeMirror.init(cm);
+	console.log("parent", parent);
+	const evalButton = parent.querySelector('.eval');
+	evalButton.onclick = function() {
+	    // newline is important before closing paren!
+	    // (in case of last line comments)
+	    const wrappedCode = "(begin " + cm.getValue() + "\n)";
+	    const res = Module.ccall('eval_string', // name of C function
+			 'string', // return type
+			 ['string'], // argument types
+			 [wrappedCode]); // arguments
+
+	    const out = Module.ccall('get_out', // name of C function
+			 'string', // return type
+			 [], // argument types
+				     []); // arguments
+
+	    const err = Module.ccall('get_err', // name of C function
+			 'string', // return type
+			 [], // argument types
+				     []); // arguments
+
+	    console.log("res:", res);
+	    console.log("out:", out);
+	    console.log("err:", err);
+	    parent.querySelector('.res').innerHTML = res;
+	    parent.querySelector('.out').innerHTML = out;
+	    parent.querySelector('.err').innerHTML = err;
+
+	    console.log("parent", parent);
+	    const possibleStaticResult = parent.nextSibling;
+	    if(possibleStaticResult.className == "example") {
+		possibleStaticResult.remove();
+	    }
+	    // note: check for <empty string> ?
+	}
+    }
+}
+
+window.onload = function() {
+    setup();
+}
